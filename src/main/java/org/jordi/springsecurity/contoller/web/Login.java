1538e7c9380f7c8b5cf2558894ab741b1f789d9f
ESCOFET TRENADO, JORDI (EXTERN) <jordi.escofet@volkswagen-groupservices.com>
2020-04-12 21:32:42 +0200

    more

---
+++ src/main/java/org/jordi/springsecurity/contoller/web/Login.java
@@ -0,0 +1,14 @@
+package org.jordi.springsecurity.contoller.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Login {
+	
+	@RequestMapping("/login")
+	public String login() {
+		return "login";
+	}
+
+}
