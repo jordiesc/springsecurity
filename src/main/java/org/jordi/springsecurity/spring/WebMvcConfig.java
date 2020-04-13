ab9ae7cfb74ef378b38e49cb97c058bfb8db0cfd
jordiesc <jordi.escofet@gmail.com>
2020-04-13 05:54:06 +0200

    more

--- src/main/java/org/jordi/springsecurity/spring/WebMvcConfig.java
+++ src/main/java/org/jordi/springsecurity/spring/WebMvcConfig.java
@@ -3,10 +3,19 @@ package org.jordi.springsecurity.spring;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = { "org.jordi.springsecurity.contoller.web"}) 
-public class WebMvcConfig {
+public class WebMvcConfig implements WebMvcConfigurer {
 
+
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/home").setViewName("home");
+		registry.addViewController("/").setViewName("home");
+		registry.addViewController("/hello").setViewName("hello");
+		registry.addViewController("/login").setViewName("login");
+	}
 }
